de9351d95fb77fc5f59659cc2b4e0c8f0b464dd6
mindmdd <mindpiano@gmail.com>
2019-11-02 14:49:36 +0700

    get username from database

--- app/src/main/java/com/example/vango_user/MainActivity.java
+++ app/src/main/java/com/example/vango_user/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -17,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private CollectionReference tripRef = database.collection("trip");
 
     private tripAdapter adapter;
+    TextView usernameDisplay;
+    String uid;
+    final String KEY_USERNAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,28 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setUpRecyclerView();
+        usernameDisplay = findViewById(R.id.usernameDisplay);
+        getUser();
+    }
+
+    private void getUser(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            uid = user.getUid();
+            //usernameDisplay.setText(uid);
+        }
+
+        DocumentReference docRef = database.collection("user").document(uid);
+        docRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            String username = documentSnapshot.getString(KEY_USERNAME);
+                            usernameDisplay.setText(username);
+                        }
+                    }
+                });
     }
 
     private void setUpRecyclerView(){
