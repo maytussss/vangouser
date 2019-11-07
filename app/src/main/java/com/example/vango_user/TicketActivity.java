7f015d2d5f212fa71f534bcf5403712235bd5e99
mindmdd <mindpiano@gmail.com>
2019-11-07 20:53:15 +0700

    gen QR success!

--- app/src/main/java/com/example/vango_user/TicketActivity.java
+++ app/src/main/java/com/example/vango_user/TicketActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +26,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class TicketActivity extends AppCompatActivity {
     private FirebaseFirestore database = FirebaseFirestore.getInstance();
@@ -32,6 +39,7 @@ public class TicketActivity extends AppCompatActivity {
     String uid;
     String tripDocId;
     TextView textView3;
+    ImageView imageView;
 
     int count = 0;
 
@@ -42,6 +50,8 @@ public class TicketActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         textView3 = this.findViewById(R.id.textView3);
+        imageView = this.findViewById(R.id.imageView);
+        genQR();
         countQueue();
 
 
@@ -63,6 +73,9 @@ public class TicketActivity extends AppCompatActivity {
                 startActivity(b2);
             }
         });
+
+
+
     }
 
     public void countQueue(){
@@ -89,7 +102,6 @@ public class TicketActivity extends AppCompatActivity {
                 }
             });*/
 
-
         CollectionReference Ref = database.collection("trip").document(tripDocId).collection("queue");
         Ref
             .orderBy("timestamp",Query.Direction.ASCENDING)
@@ -109,5 +121,20 @@ public class TicketActivity extends AppCompatActivity {
                     }
                 }
             });
+    }
+
+    public void genQR(){
+        SharedPreferences ticketPref = getSharedPreferences("ticket",MODE_PRIVATE);
+        tripDocId = ticketPref.getString("ticket","");
+        String text= tripDocId;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
