1419a0db0bcf57200bc5217c12c97b7aeffd3130
ssanpoom <56549952+ssanpoom@users.noreply.github.com>
2019-11-07 21:59:03 +0700

    create

---
+++ vangoservice/VanGo/userData.swift
@@ -0,0 +1,32 @@
+//
+//  userData.swift
+//  VanGo
+//
+//  Created by sanpoom punapanont on 29/10/2562 BE.
+//  Copyright © 2562 sanpoom punapanont. All rights reserved.
+//
+
+//import Foundation
+//class userData
+//{
+//    static let start = "start"
+//    static let stop = "stop"
+//
+//    private var _start:String! = ""
+//    private var _stop:String! = ""
+//
+//    var Start:String
+//    {
+//        return _start
+//    }
+//    var Stop:String
+//    {
+//        return _stop
+//    }
+//
+//    init(START :String,STOP:String)
+//    {
+//        self._start = START
+//        self._stop = STOP
+//    }
+//}
