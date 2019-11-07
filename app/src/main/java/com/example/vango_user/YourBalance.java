ac98a6aa982c3439b9e7ffbab7b434394ed88a61
mindmdd <mindpiano@gmail.com>
2019-11-08 02:09:07 +0700

    Revert "Edit Balance UI"
    This reverts commit be79836f6e0ff8b436a1d50e0805dd53cf4f7d22.

--- app/src/main/java/com/example/vango_user/YourBalance.java
+++ app/src/main/java/com/example/vango_user/YourBalance.java
@@ -3,7 +3,6 @@ package com.example.vango_user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +20,6 @@ public class YourBalance extends AppCompatActivity {
     String uid;
     private FirebaseFirestore database = FirebaseFirestore.getInstance();
     TextView balance_amount;
-    TextView UserName;
-    SharedPreferences sp;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,62 +27,8 @@ public class YourBalance extends AppCompatActivity {
         setContentView(R.layout.activity_your_balance);
         getSupportActionBar().hide();
 
-        Button uni25btn = findViewById(R.id.uni25btn);
-        Button uni50btn = findViewById(R.id.uni50btn);
-        Button uni75btn = findViewById(R.id.uni75btn);
-        Button uni100btn = findViewById(R.id.uni100btn);
-        Button uni200btn = findViewById(R.id.uni200btn);
-        Button uni500btn = findViewById(R.id.uni500btn);
-        Button uni1000btn = findViewById(R.id.uni1000btn);
-
-
         balance_amount = findViewById(R.id.balance_amount);
-        UserName = findViewById(R.id.UserName);
         getUser();
-
-        uni25btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(25);
-            }
-        });
-        uni50btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(50);
-            }
-        });
-        uni75btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(75);
-            }
-        });
-        uni100btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(100);
-            }
-        });
-        uni200btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(200);
-            }
-        });
-        uni500btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                updateCoin(500);
-            }
-        });
-        uni1000btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){ updateCoin(1000);
-            }
-        });
-
-
     }
     public void onBackPressed() {
         Intent intent = new Intent(YourBalance.this, MainActivity.class);
@@ -112,17 +54,8 @@ public class YourBalance extends AppCompatActivity {
                         if (documentSnapshot.exists()) {
                             Double coin = documentSnapshot.getDouble("coin");
                             balance_amount.setText(String.valueOf(coin));
-                            String name = documentSnapshot.getString("name");
-                            UserName.setText(name);
                         }
                     }
                 });
-    }
-
-    public void updateCoin(final int price){
-        Intent intent = new Intent(YourBalance.this, AddMoneySuccess.class);
-        intent.putExtra("price", price);
-        startActivity(intent);
-        finish();
     }
 }
