1a7e27a64bbbe3f0e8f9d254ad4507878889a1cc
ssanpoom <56549952+ssanpoom@users.noreply.github.com>
2019-11-09 20:50:47 +0700

    create

--- vangoservice/VanGo/userData.swift
+++ vangoservice/VanGo/userData.swift
@@ -6,27 +6,27 @@
 //  Copyright © 2562 sanpoom punapanont. All rights reserved.
 //
 
-//import Foundation
-//class userData
-//{
-//    static let start = "start"
-//    static let stop = "stop"
-//
-//    private var _start:String! = ""
-//    private var _stop:String! = ""
-//
-//    var Start:String
-//    {
-//        return _start
-//    }
-//    var Stop:String
-//    {
-//        return _stop
-//    }
-//
-//    init(START :String,STOP:String)
-//    {
-//        self._start = START
-//        self._stop = STOP
-//    }
-//}
+import Foundation
+class userData
+{
+    static let start = "start"
+    static let stop = "stop"
+
+    private var _start:String! = ""
+    private var _stop:String! = ""
+
+    var Start:String
+    {
+        return _start
+    }
+    var Stop:String
+    {
+        return _stop
+    }
+
+    init(START :String,STOP:String)
+    {
+        self._start = START
+        self._stop = STOP
+    }
+}
