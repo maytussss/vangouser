d9bfa4df7db937f47475971ffff7d5c07386867d
maytussss <maytus.udomlerd@github.com>
2019-11-11 16:22:51 +0700

    push

---
+++ app/src/main/java/com/example/vango_user/not_correct_qrcode.java
@@ -0,0 +1,15 @@
+package com.example.vango_user;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class not_correct_qrcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_correct_qrcode);
+    }
+}
