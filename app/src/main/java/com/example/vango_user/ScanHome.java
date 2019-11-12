935d65fe0d8a44a69bea20045125e79b274e1e0a
mindmdd <mindpiano@gmail.com>
2019-11-13 00:36:54 +0700

    no qr code found

--- app/src/main/java/com/example/vango_user/ScanHome.java
+++ app/src/main/java/com/example/vango_user/ScanHome.java
@@ -8,18 +8,30 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
 import info.androidhive.barcode.BarcodeReader;
 
 public class ScanHome extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener{
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     BarcodeReader barcodeReader;
     SharedPreferences sp;
+    int check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,24 +42,31 @@ public class ScanHome extends AppCompatActivity implements BarcodeReader.Barcode
     }
     @Override
     public void onScanned(Barcode barcode) {
-
         barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
 
         // Check detected text is empty?
         if (TextUtils.isEmpty(barcode.displayValue)) {
             //Toast.makeText(getApplicationContext(), "Barcode is empty!", Toast.LENGTH_LONG).show();
         }
-        else
-            {
-            // ticket details activity by passing barcode
-            String code =  barcode.displayValue;
-            sp = getSharedPreferences("barcode",MODE_PRIVATE);
-            sp.edit().putString("barcode",code).apply();
+        else {
 
-            Intent intent = new Intent(ScanHome.this, BillPayment.class);
-            intent.putExtra("code", barcode.displayValue);
-            startActivity(intent);
-            finish();
+            int tripExist = getTripDetail(barcode.displayValue);
+            if (tripExist == 1) {
+                String code = barcode.displayValue;
+                sp = getSharedPreferences("barcode", MODE_PRIVATE);
+                sp.edit().putString("barcode", code).apply();
+
+
+                Intent intent = new Intent(ScanHome.this, BillPayment.class);
+                intent.putExtra("code", barcode.displayValue);
+                startActivity(intent);
+                finish();
+            }
+            else{
+                Intent intent = new Intent(ScanHome.this, not_correct_qrcode.class);
+                startActivity(intent);
+                finish();
+            }
         }
     }
 
@@ -76,5 +95,25 @@ public class ScanHome extends AppCompatActivity implements BarcodeReader.Barcode
         finish();
     }
 
+    private int getTripDetail(String tripDocId){
+        DocumentReference docRef = database.collection("trip").document(tripDocId);
+        docRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            check = 1;
+                        }
+                    }
+                });
+        docRef.get()
+                .addOnFailureListener(new OnFailureListener()  {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                            check = 0;
+                    }
+                });
+        return check;
+    }
 }
 
